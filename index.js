32d4801ac21d26d790c768428c5799a6af11a299
Yusuf Çolak <yusuf21706@gmail.com>
2023-03-23 22:05:56 +0300

    aaa

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ const serpmeKahvalti = {isim: "Serpme Kahvaltı", fiyat: 16, kategori:"Kahvaltı
 */
 
 
-function MenuElemaniOlustur(/*Kodlar buraya*/){
-	/*Kodlar buraya*/
-}
+function MenuElemaniOlustur(isim, fiyat, kategori){
 
-
+	return {isim, fiyat, kategori};
+	
+	}
+	console.log(MenuElemaniOlustur("Cheeseburger",8, "Burgerler")); 
 
 /*  Görev 1b (otomatik test yok): 
 	Fonksiyonu çağırın!
@@ -30,7 +31,12 @@ function MenuElemaniOlustur(/*Kodlar buraya*/){
 	
 	Örnek: MenuElemaniOlustur("Karışık Pizza",5,"Pizzalar") şunu döndürür: {isim:"Karışık Pizza",fiyat:5,kategori:"Pizzalar"}
 */
-
+const lahmacun = MenuElemaniOlustur("Lahmacun", 3, "Aperatifler");
+console.log(lahmacun);
+const tavukluSalata = MenuElemaniOlustur("Tavuklu Salata", 14, "Salatalar");
+console.log(tavukluSalata);
+const kofte = MenuElemaniOlustur("Izgara Köfte", 18, "Ana Yemekler");
+console.log(kofte);
 
 
 /* Görev 2: 
@@ -44,6 +50,59 @@ function MenuElemaniOlustur(/*Kodlar buraya*/){
 	
 	Örnek: burger.indirim("öğretmen") 13.5 döndürmeli ve burger.indirim("diğer") 16.2 döndürmeli
 */
+/*
+const burger = {
+
+	isim: "Burger",
+	
+	fiyat: 18,
+	
+	kategori: "Öğle Yemeği",
+	
+	indirim: function(yetki) {
+	
+	let indirimOrani = 0;
+	
+	switch (yetki) {
+	
+	case "öğretmen":
+	
+	case "öğrenci":
+	
+	indirimOrani = 0.25;
+	
+	break;
+	
+	case "diğer":
+	
+	indirimOrani = 0.1;
+	
+	break;
+	
+	default:
+	
+	break;
+	
+	}
+	
+	const indirimMiktari = this.fiyat * indirimOrani;
+	
+	const indirimliFiyat = this.fiyat - indirimMiktari;
+	
+	return indirimliFiyat;
+	
+	}
+	
+	};
+	console.log(burger.indirim("öğretmen")); // 13.5
+	
+	console.log(burger.indirim("öğrenci")); // 13.5
+	
+	console.log(burger.indirim("diğer")); // 16.2
+
+*/
+
+
 
 
 const burger = {
@@ -51,8 +110,23 @@ const burger = {
 	fiyat: 18, 
 	kategori: "Öğle Yemeği", 
 
-}
+indirim: function (yetki,indirimMiktarı,YeniFiyat){
 
+if(yetki=="öğrenci" || yetki=="öğretmen")
+indirimMiktarı = burger.fiyat * 0.25;
+
+ else
+indirimMiktarı = burger.fiyat *0.1;
+
+ YeniFiyat = burger.fiyat-indirimMiktarı;
+
+ return YeniFiyat; 
+
+}
+}
+console.log(burger.indirim("öğrenci"));
+console.log(burger.indirim("öğretmen"));
+console.log(burger.indirim("diğer"));
 
 
 ///////////////Değerlendirmeler (MVP)///////////////////
@@ -71,7 +145,7 @@ const degerlendirmeler = [
 	Yukarıdaki degerlendirmeler dizisini(array) kullanarak:
 	1. Sadece Ahmet'in geribildirimini konsolda görüntüleyin - fonksiyona gerek yok
 */
-
+console.log(degerlendirmeler[5].geribildirim);
 
 
 /*  Görev 4 (ototest yok):  
@@ -79,7 +153,8 @@ const degerlendirmeler = [
 	1. Bu geribildirimi Reyna'nın değerlendirmesine ekleyin - "bu mekan bir harika dostum, yine de garsonun gülümsememesinden puan kırdım"
 	2. degerlendirmeler dizisini konsolda görüntüleyerek çalışmanızı kontrol edin
 */
-
+degerlendirmeler[7].geribildirim="bu mekan bir harika dostum, yine de garsonun gülümsememesinden puan kırdım";
+console.log(degerlendirmeler[7].isim,"'nın yorumu: ",degerlendirmeler[7].geribildirim);
 
 
 /*  Görev 5: 
@@ -94,10 +169,14 @@ const degerlendirmeler = [
 */
 
 
-function DegerledirmeEkle(/*Kodlar buraya */){
-	/*Kodlar buraya */
-	
+function DegerledirmeEkle(degerlendirmeler,isim,puan,geribildirim){
+
+const yenidegerlendirme ={isim,puan,geribildirim};
+degerlendirmeler.push(yenidegerlendirme);
+return degerlendirmeler;
 }
+console.log(DegerledirmeEkle(degerlendirmeler,"Hurşut",2,"Boktan yemekler!"))
+console.log(degerlendirmeler)
 
 
 
@@ -112,10 +191,12 @@ function DegerledirmeEkle(/*Kodlar buraya */){
 */
 
 
-function AnahtardanDegerlendirmeAl(/*Kodlar buraya*/) {
-	/*Kodlar buraya*/
+function AnahtardanDegerlendirmeAl(degerlendirmeler, index) {
+	const degerlendirme = degerlendirmeler[index];
+	return degerlendirme.isim + " isimli kişi " + degerlendirme.puan + " puan verdi ve şunları yazdı: " + degerlendirme.geribildirim;
+  }
 
-}
+console.log(AnahtardanDegerlendirmeAl(degerlendirmeler,0))
 
 
 
@@ -131,11 +212,11 @@ function AnahtardanDegerlendirmeAl(/*Kodlar buraya*/) {
 	Not: Eğer 4. görevi başarıyla yaptıysanız kişinin geribildirimi boş görünmemeli
 */
 
-
-function SonDegerlendirmeyiAl(/*Kodlar buraya*/) {
-	/*Kodlar buraya*/
-} 
-
+function SonDegerlendirmeyiAl(degerlendirmeler){
+ const sonDegerlendirme  = degerlendirmeler[degerlendirmeler.length - 1];
+	return sonDegerlendirme.isim + " isimli kişi " + sonDegerlendirme.puan + " puan verdi ve şunları yazdı: " + sonDegerlendirme.geribildirim;
+}
+console.log(SonDegerlendirmeyiAl(degerlendirmeler));
 
 
 /////////////// BONUS  GÖRVLER////////////////////
